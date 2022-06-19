6a2a119060fff6208de1d074adae7fa8c228178c
Ahmed Mahmoud <86148334+AhmedmMhmoud2001@users.noreply.github.com>
2022-06-19 23:57:22 +0200

    Add files via upload

---
+++ js/index.js
@@ -0,0 +1,6 @@
+var typed = new Typed(".typing" , {
+    strings:["Web Developer","Front-End Developer" , "Front-End Engineer" ,"CSS/HTML Developer","Full-stack JavaScript Developer"],
+    typedSpeed : 100 ,
+    backSpeed :60 ,
+    loop :true
+})
